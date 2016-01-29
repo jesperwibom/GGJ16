355fd8bd65504a4e2083d00b3df2e33934fcdef1
Jesper Andersson <jsprA86@gmail.com>
2016-01-30 00:25:22 +0100

    Added player ctrl and fire scripts

---
+++ Assets/Scripts/Player/PlayerControl.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+	public float moveSpeed = 12f;
+	/*
+	public AudioSource moveAudio; 
+	public AudioClip playerNotMoving;
+	public AudioClip playerIsMoving;
+	*/
+
+	private Rigidbody2D rb;
+	private float movementInput;
+
+	void Awake(){
+		rb = GetComponent<Rigidbody2D> ();
+	}
+
+	void OnEnable(){
+		movementInput = 0f;
+	}
+
+	void OnDisable(){
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		movementInput = Input.GetAxis ("Vertical");
+	}
+
+	void FixedUpdate(){
+		Move ();
+	}
+
+	void Move(){
+		Vector2 movement = new Vector2 (0,movementInput * moveSpeed * Time.deltaTime);
+		rb.MovePosition(rb.position + movement);
+	}
+}
---
+++ Assets/Scripts/Player/WeaponFire.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeaponFire : MonoBehaviour {
+
+	private bool btnPressed;
+	private bool fired;
+
+	// Update is called once per frame
+	void Update () {
+		btnPressed = Input.GetButton ("Jump"); //gets the space key
+	}
+
+	void FixedUpdate(){
+		if (!btnPressed) {
+			fired = false;
+		}
+		if (btnPressed && !fired) {
+			Fire ();
+		}
+	}
+
+	void Fire(){
+		fired = true;
+		Debug.Log ("Instanciate bullet here");
+
+	}
+}
