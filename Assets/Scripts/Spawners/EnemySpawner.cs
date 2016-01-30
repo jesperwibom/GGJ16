6b83aec355dd9e60dcaed003275a484f55e2a811
Mattias Andersson <mattias8012@gmail.com>
2016-01-30 18:48:19 +0100

    spawner update

--- Assets/Scripts/Spawners/EnemySpawner.cs
+++ Assets/Scripts/Spawners/EnemySpawner.cs
@@ -5,6 +5,7 @@ public class EnemySpawner : MonoBehaviour {
 	public int NumberOfEnemies;
 	private int EnemyCounter;
 	private bool bossSpawned;
+	public GameObject[] enemyTypes;
 
 	public Rigidbody2D enemy;
 	public Rigidbody2D boss;
@@ -26,6 +27,7 @@ public class EnemySpawner : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		
 		spawnPosition  = gameObject.transform;
 		StartCoroutine (Spawner());
 
@@ -38,16 +40,9 @@ public class EnemySpawner : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (EnemyCounter == NumberOfEnemies) {
-
-
-			// SpawnBoss ();
-			backgroundScroller.stopScroll ();
-			forgroundScroller.stopScroll ();
-			middlegroundScroller.stopScroll ();
-			if (bossSpawned == false) {
-				StartCoroutine (BossSpawner ());
-			}
+		if (EnemyCounter > enemyTypes.Length) {
+			StopCoroutine (Spawner());
+			StartCoroutine (stopScrolls ());
 		}
 
 	}
@@ -55,22 +50,35 @@ public class EnemySpawner : MonoBehaviour {
 	private IEnumerator Spawner (){
 		yield return new WaitForSeconds (interval);
 
+		int arrayPos = Random.Range(0, enemyTypes.Length); 
 		int randomY = Random.Range(-3, 4); 
 		spawnPos = new Vector3 (spawnPosition.position.x, randomY, 4);
-
+		enemy = enemyTypes [arrayPos].GetComponent<Rigidbody2D>();
 		Rigidbody2D enemyIns = Instantiate (enemy, spawnPos, spawnPosition.rotation) as Rigidbody2D; 
-
+		Debug.Log ("Spawned " + enemy);
 
 		// fixed pos
 		// Rigidbody2D enemyIns = Instantiate (enemy, spawnPosition.position, spawnPosition.rotation) as Rigidbody2D; 
-		if (bossSpawned == false && EnemyCounter < NumberOfEnemies) {
+		if (bossSpawned == false && EnemyCounter < enemyTypes.Length) {
 			StartCoroutine (Spawner());
 		}
 		EnemyCounter = EnemyCounter + 1;
 
 	}
+
+	private IEnumerator stopScrolls (){
+		yield return new WaitForSeconds (5);
+		if (bossSpawned == false) {
+			StartCoroutine (BossSpawner ());
+
+			// SpawnBoss ();
+			backgroundScroller.stopScroll ();
+			forgroundScroller.stopScroll ();
+			middlegroundScroller.stopScroll ();
+		}
+	}
 	private IEnumerator BossSpawner (){
-		yield return new WaitForSeconds (8f);
+		yield return new WaitForSeconds (3);
 		SpawnBoss ();
 		StopAllCoroutines ();
 	}
