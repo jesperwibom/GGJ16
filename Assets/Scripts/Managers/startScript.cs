cf06a58c2009f43b83e58251f5580ed8715c8c8f
Manne Westermark <mannewestermark@gmail.com>
2016-01-30 20:40:15 +0100

    Startscreen and endscreen

---
+++ Assets/Scripts/Managers/startScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class startScript : MonoBehaviour {
+
+	public GameObject rend;
+	bool blinking = true;
+	// Use this for initialization
+	void Start () {
+		rend = gameObject;
+		StartCoroutine (BlinkText ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(blinking == false){
+			rend.GetComponent<Renderer> ().enabled = true;
+		} else if(blinking == true){
+			rend.GetComponent<Renderer> ().enabled = false;
+		}
+	}
+
+	private IEnumerator BlinkText(){
+		yield return new WaitForSeconds (1);
+		blinking = !blinking;
+		StartCoroutine(BlinkText());
+	}
+}
