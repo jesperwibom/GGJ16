f165738cf5ad004e2b1bce79e44be502cc6ec40a
Mattias Andersson <mattias8012@gmail.com>
2016-01-30 12:04:17 +0100

    parallax

---
+++ Assets/Scripts/Levels/ScrollScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollScript : MonoBehaviour {
+	public float speed = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.GetComponent<Renderer> ().material.mainTextureOffset = new Vector2 (Time.time * speed, 0f);
+
+	}
+}
